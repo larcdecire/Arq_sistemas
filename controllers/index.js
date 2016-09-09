12a899bb7e868f8408cc0b39ec029c4db732a228
larcdecire <onlynoob_92@hotmail.com>
2016-09-09 13:29:31 -0300

    update

--- controllers/index.js
+++ controllers/index.js
@@ -15,7 +15,8 @@ module.exports = function (router) {
     
     router.post('/', function (req, res)){
         var x = req.body;
-        res.send('<code><pre>' + raq.body + '</pre></code>');
-        res.status(200).end;
+        r//es.send('<code><pre>' + raq.body + '</pre></code>');
+        //res.status(200).end;
+        res.status(200).json(x).end();
     });
 };
