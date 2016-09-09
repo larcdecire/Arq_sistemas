325f119b6d832c66b861fc55bb31c8e654ff6b16
Jean Luis Contreras Leyton <jean123447@gmail.com>
2016-09-09 13:12:42 -0300

    update3

--- controllers/index.js
+++ controllers/index.js
@@ -12,5 +12,9 @@ module.exports = function (router) {
         res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
         
     });
-
+    
+    router.post('/', function (req, res)){
+        var x = req.body;
+        res.send('<code><pre>' + raq.body + '</pre></code>')
+    }
 };
